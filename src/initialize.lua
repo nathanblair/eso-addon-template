9c33787a4a267f587d0941ac0b3e4d3404d4737e
Nathan Blair <me@nathanblair.rocks>
2023-01-16 23:02:36 -0500

    generate all boilerplate

--- src/initialize.lua
+++ src/initialize.lua
@@ -1,15 +1,4 @@
-Wombat = {}
-
-Wombat.name = "Wombat"
-
 function Wombat.Initialize()
     -- Hook into events that this addon will handle
     -- Namely, any combat-state-change events
-end
-
-function Wombat.OnAddOnLoaded(_, addonName)
-    if addonName == Wombat.name then
-        Wombat.Initialize()
-        EVENT_MANAGER:UnregisterForEvent(Wombat.name, EVENT_ADD_ON_LOADED)
-    end
 end
